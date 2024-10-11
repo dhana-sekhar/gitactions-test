112020a66eec8c923b1f7daf0e0a56f8484f26af
Sekhar <buddha.dhanasekhar@gmail.com>
2024-10-11 16:59:52 +0800

    First commit for git actions

---
+++ src/mathf.py
@@ -0,0 +1,22 @@
+class Mathfunc:
+
+    def adding(a: int,b: int) -> int:
+        """This is an addition math function
+
+        Returns:
+            _type_: returns sum of given 2 parametes
+        """
+
+        return a+b
+
+    def subt(a: int,b: int) -> int: 
+        """This is an Sub math function
+
+        Args:
+            a (int): first value
+            b (int): second value
+
+        Returns:
+            _type_: reuturns sub of given 2 parameters 
+        """
+        return a-b
---
+++ tests/operations_test.py
@@ -0,0 +1,16 @@
+from src.mathf import Mathfunc
+
+def test_add():
+    """testing add func
+    """
+    assert Mathfunc.adding(2,3)==5
+    assert Mathfunc.adding(-245, 3)==-242
+
+
+def test_sub():
+    """Testing sub func
+    """
+    assert Mathfunc.subt(5,3)==2
+    assert Mathfunc.subt(4,3)==1
+    assert Mathfunc.subt(3,3)==0
+    assert Mathfunc.subt(2,3)==-1
